336d67ae8f4e69dd0fe59c0f7053edfb3e585b76
luoam <609892502@qq.com>
2018-07-17 14:06:35 +0800

    edit spider

--- ttlxspider/ttlxspider/spiders/media.py
+++ ttlxspider/ttlxspider/spiders/media.py
@@ -3,11 +3,20 @@ import scrapy
 
 from ttlxspider.items import TtlxspiderItem
 
+
 class MediaSpider(scrapy.Spider):
     name = 'media'
     allowed_domains = ['oss.6tiantian.com', '127.0.0.1']
     start_urls = ['http://127.0.0.1:9501/index/Urls/1']
 
     def parse(self, response):
+        url = TtlxspiderItem()
         for item in response.xpath('/html/body/div/ul/li/a/@href').extract():
-            print(item)
+            url['mediaurl'] = item
+            yield url
+
+        weburl = "http://127.0.0.1:9501%s"
+        next_url = response.xpath('//a[contains(text(),"下一页")]/@href').extract_first()
+        next_url = weburl % next_url
+        print(next_url)
+        yield scrapy.Request(next_url, callback=self.parse)
